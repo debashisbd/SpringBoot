3dc2d343b787a9f8c72e87ef076c537af5d69a02
Debashis Baidya <bddebashis@gmail.com>
2022-08-14 14:42:59 +0200

    JPA part added to use database

---
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.student;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student, String> {
+
+	
+
+}
